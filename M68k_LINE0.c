1fff91996e6e2f2ef93dba6657a55159827743bb
Michal Schulz <michal.schulz@gmx.de>
2019-02-01 07:05:23 +0100

    addi implemented

--- M68k_LINE0.c
+++ M68k_LINE0.c
@@ -8,6 +8,7 @@
 uint32_t *EMIT_line0(uint32_t *ptr, uint16_t **m68k_ptr)
 {
     uint16_t opcode = (*m68k_ptr)[0];
+    (*m68k_ptr)++;
 
     if ((opcode & 0xff00) == 0x0000 && (opcode & 0x00c0) != 0x00c0)   /* 00000000xxxxxxxx - ORI to CCR, ORI to SR, ORI */
     {
@@ -23,7 +24,55 @@ uint32_t *EMIT_line0(uint32_t *ptr, uint16_t **m68k_ptr)
     }
     else if ((opcode & 0xff00) == 0x0600 && (opcode & 0x00c0) != 0x00c0)   /* 00000110xxxxxxxx - ADDI */
     {
+        uint8_t ext_count = 0;
+        uint8_t tmp_count = 0;
+        uint8_t immed = RA_AllocARMRegister(&ptr);
+        uint8_t dest;
+        uint8_t size = 0;
 
+        /* Load immediate into the register */
+        switch (opcode & 0x00c0)
+        {
+            case 0x0000:    /* Byte operation */
+                *ptr++ = ldrb_offset(REG_PC, immed, 3);
+                *ptr++ = lsl_immed(immed, immed, 24);
+                ext_count++;
+                size = 1;
+                break;
+            case 0x0040:    /* Short operation */
+                *ptr++ = ldrh_offset(REG_PC, immed, 2);
+                *ptr++ = lsl_immed(immed, immed, 16);
+                ext_count++;
+                size = 2;
+                break;
+            case 0x0080:    /* Long operation */
+                *ptr++ = ldr_offset(REG_PC, immed, 2);
+                ext_count+=2;
+                size = 4;
+                break;
+        }
+
+        /* Fetch data from destination */
+        tmp_count = ext_count;
+        ptr = EMIT_LoadFromEffectiveAddress(ptr, size, &dest, opcode & 0x3f, *m68k_ptr, &ext_count);
+
+        /* Perform calcualtion */
+        *ptr++ = adds_reg(immed, immed, dest, 24);
+        *ptr++ = lsr_immed(immed, immed, 24);
+
+        /* Store back to destination */
+        ptr = EMIT_StoreToEffectiveAddress(ptr, size, &immed, opcode & 0x3f, *m68k_ptr, &tmp_count);
+
+        RA_FreeARMRegister(&ptr, immed);
+
+        *ptr++ = add_immed(REG_PC, REG_PC, 2 * (ext_count + 1));
+        (*m68k_ptr) += ext_count;
+
+        *ptr++ = bic_immed(REG_SR, REG_SR, SR_X | SR_C | SR_V | SR_Z | SR_N);
+        *ptr++ = or_cc_immed(ARM_CC_CS, REG_SR, REG_SR, SR_X | SR_C);
+        *ptr++ = or_cc_immed(ARM_CC_VS, REG_SR, REG_SR, SR_V);
+        *ptr++ = or_cc_immed(ARM_CC_EQ, REG_SR, REG_SR, SR_Z);
+        *ptr++ = or_cc_immed(ARM_CC_MI, REG_SR, REG_SR, SR_N);
     }
     else if ((opcode & 0xf9c0) == 0x00c0)   /* 00000xx011xxxxxx - CMP2, CHK2 */
     {
