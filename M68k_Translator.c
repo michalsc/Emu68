4becb13652efc4b5fb7380c790faa1dd79d31eb3
Michal Schulz <michal.schulz@gmx.de>
2019-02-02 14:17:36 +0100

    fixed endianess

--- M68k_Translator.c
+++ M68k_Translator.c
@@ -17,7 +17,7 @@ void *handle;
 uint32_t *EmitINSN(uint32_t *arm_ptr, uint16_t **m68k_ptr)
 {
     uint32_t *ptr = arm_ptr;
-    uint16_t opcode = (*m68k_ptr)[0];
+    uint16_t opcode = BE16((*m68k_ptr)[0]);
     uint8_t group = opcode >> 12;
 
     if (group == 0)
