8b0c4e3b5a688eddb9685b00ea3fa4abba0b6451
Michal Schulz <michal.schulz@gmx.de>
2019-02-02 14:17:27 +0100

    fixed endianess

--- M68k_MOVE.c
+++ M68k_MOVE.c
@@ -7,7 +7,7 @@
 
 uint32_t *EMIT_moveq(uint32_t *ptr, uint16_t **m68k_ptr)
 {
-    uint16_t opcode = (*m68k_ptr)[0];
+    uint16_t opcode = BE16((*m68k_ptr)[0]);
     int8_t value = opcode & 0xff;
     uint8_t reg = (opcode >> 9) & 7;
     uint8_t tmp_reg = RA_MapM68kRegisterForWrite(&ptr, reg);
@@ -17,7 +17,7 @@ uint32_t *EMIT_moveq(uint32_t *ptr, uint16_t **m68k_ptr)
     *ptr++ = movs_immed_s8(tmp_reg, value);
     *ptr++ = add_immed(REG_PC, REG_PC, 2);
 
-    uint8_t mask = M68K_GetSRMask((*m68k_ptr)[0]);
+    uint8_t mask = M68K_GetSRMask(BE16((*m68k_ptr)[0]));
     uint8_t update_mask = (SR_C | SR_V | SR_Z | SR_N) & ~mask;
 
     if (update_mask)
@@ -35,7 +35,7 @@ uint32_t *EMIT_moveq(uint32_t *ptr, uint16_t **m68k_ptr)
 
 uint32_t *EMIT_move(uint32_t *ptr, uint16_t **m68k_ptr)
 {
-    uint16_t opcode = (*m68k_ptr)[0];
+    uint16_t opcode = BE16((*m68k_ptr)[0]);
     uint8_t ext_count = 0;
     uint8_t tmp_reg = 0xff;
     uint8_t size = 0;
@@ -65,7 +65,7 @@ uint32_t *EMIT_move(uint32_t *ptr, uint16_t **m68k_ptr)
 
     if (!is_movea)
     {
-        uint8_t mask = M68K_GetSRMask((*m68k_ptr)[0]);
+        uint8_t mask = M68K_GetSRMask(BE16((*m68k_ptr)[0]));
         uint8_t update_mask = (SR_C | SR_V | SR_Z | SR_N) & ~mask;
 
         if (update_mask)
