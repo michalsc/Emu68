4e04bf5f60a06dea7708f9084dafd6df16f256f6
Michal Schulz <michal.schulz@gmx.de>
2019-02-03 13:19:40 +0100

    add config file with parameters

---
+++ config.h
@@ -0,0 +1,9 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+#define EMU68_ARM_CACHE_SIZE    (4*1024*1024)
+#define EMU68_M68K_INSN_DEPTH   64
+#define EMU68_HOST_BIG_ENDIAN   0
+#define EMU68_HAS_SETEND        1
+
+#endif /* _CONFIG_H */
