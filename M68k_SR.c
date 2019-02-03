11e0684fda6ca95407b06e9270a2612104815006
Michal Schulz <michal.schulz@gmx.de>
2019-02-03 11:01:37 +0100

    add line4 to SR evaluation

--- M68k_SR.c
+++ M68k_SR.c
@@ -69,7 +69,151 @@ uint8_t M68K_GetSRMask(uint16_t opcode)
     {
         mask = SR_C | SR_Z | SR_N | SR_V;
     }
-
+    /* MOVE from SR */
+    else if ((opcode & 0xffc0) == 0x40c0)
+    {
+    }
+    /* MOVE from CCR */
+    else if ((opcode & 0xffc0) == 0x42c0)
+    {
+    }
+    /* NEGX */
+    else if ((opcode & 0xff00) == 0x4000 && (opcode & 0xc0) != 0xc0)
+    {
+    }
+    /* CLR */
+    else if ((opcode & 0xff00) == 0x4200 && (opcode & 0xc0) != 0xc0)
+    {
+    }
+    /* MOVE to CCR */
+    else if ((opcode & 0xffc0) == 0x44c0)
+    {
+    }
+    /* NEG */
+    else if ((opcode & 0xff00) == 0x4400 && (opcode & 0xc0) != 0xc0)
+    {
+    }
+    /* MOVE to SR */
+    else if ((opcode & 0xffc0) == 0x46c0)
+    {
+    }
+    /* NOT */
+    else if ((opcode & 0xff00) == 0x4600 && (opcode & 0xc0) != 0xc0)
+    {
+    }
+    /* EXT, EXTB */
+    else if ((opcode & 0xfe38) == 0x4808)
+    {
+    }
+    /* LINK */
+    else if ((opcode & 0xfff8) == 0x4808)
+    {
+    }
+    /* NBCD */
+    else if ((opcode & 0xffc0) == 0x4800 && (opcode & 0x08) != 0x08)
+    {
+    }
+    /* SWAP */
+    else if ((opcode & 0xfff8) == 0x4840)
+    {
+        mask = SR_Z | SR_N | SR_C | SR_V;
+    }
+    /* BKPT */
+    else if ((opcode & 0xfff8) == 0x4848)
+    {
+    }
+    /* 0100100001xxxxxx - PEA */
+    else if ((opcode & 0xffc0) == 0x4840 && (opcode & 0x38) != 0x08)
+    {
+    }
+    /* 0100101011111100 - ILLEGAL */
+    else if (opcode == 0x4afc)
+    {
+    }
+    /* 0100101011xxxxxx - TAS */
+    else if ((opcode & 0xffc0) == 0x4ac0)
+    {
+    }
+    /* 0100101011xxxxxx - TST */
+    else if ((opcode & 0xff00) == 0x4a00 && (opcode & 0xc0) != 0xc0)
+    {
+    }
+    /* 0100110000xxxxxx - MULU, MULS, DIVU, DIVUL, DIVS, DIVSL */
+    else if ((opcode & 0xff80) == 0x4a00)
+    {
+    }
+    /* 010011100100xxxx - TRAP */
+    else if ((opcode & 0xfff0) == 0x4e40)
+    {
+    }
+    /* 0100111001010xxx - LINK */
+    else if ((opcode & 0xfff8) == 0x4e50)
+    {
+    }
+    /* 0100111001011xxx - UNLK */
+    else if ((opcode & 0xfff8) == 0x4e58)
+    {
+    }
+    /* 010011100110xxxx - MOVE USP */
+    else if ((opcode & 0xfff0) == 0x4e60)
+    {
+    }
+    /* 0100111001110000 - RESET */
+    else if (opcode == 0x4e70)
+    {
+    }
+    /* 0100111001110000 - NOP */
+    else if (opcode == 0x4e71)
+    {
+    }
+    /* 0100111001110010 - STOP */
+    else if (opcode == 0x4e72)
+    {
+    }
+    /* 0100111001110011 - RTE */
+    else if (opcode == 0x4e73)
+    {
+    }
+    /* 0100111001110100 - RTD */
+    else if (opcode == 0x4e74)
+    {
+    }
+    /* 0100111001110101 - RTS */
+    else if (opcode == 0x4e75)
+    {
+    }
+    /* 0100111001110110 - TRAPV */
+    else if (opcode == 0x4e76)
+    {
+    }
+    /* 0100111001110111 - RTR */
+    else if (opcode == 0x4e77)
+    {
+    }
+    /* 010011100111101x - MOVEC */
+    else if ((opcode & 0xfffe) == 0x4e7a)
+    {
+    }
+    /* 0100111010xxxxxx - JSR */
+    else if ((opcode & 0xffc0) == 0x4e80)
+    {
+    }
+    /* 0100111011xxxxxx - JMP */
+    else if ((opcode & 0xffc0) == 0x4ec0)
+    {
+    }
+    /* 01001x001xxxxxxx - MOVEM */
+    else if ((opcode & 0xfb80) == 0x4880)
+    {
+    }
+    /* 0100xxx111xxxxxx - LEA */
+    else if ((opcode & 0xf1c0) == 0x41c0)
+    {
+    }
+    /* 0100xxx1x0xxxxxx - CHK */
+    else if ((opcode & 0xf140) == 0x4100)
+    {
+    }
 
     return mask;
 }
