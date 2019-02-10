a9090aab1fad7398b83c2ea1be4c46a988a69afc
Michal Schulz <michal.schulz@gmx.de>
2019-02-10 16:12:55 +0100

    CMP/CMPM/CMPA implemented

--- M68k_LINEB.c
+++ M68k_LINEB.c
@@ -11,8 +11,148 @@ uint32_t *EMIT_lineB(uint32_t *ptr, uint16_t **m68k_ptr)
     uint16_t opcode = BE16((*m68k_ptr)[0]);
     (*m68k_ptr)++;
 
+    /* 1011xxx1xx001xxx - CMPM */
+    if ((opcode & 0xf138) == 0xb108)
+    {
+        uint8_t size = 1 << ((opcode >> 6) & 3);
+        uint8_t src = 0xff;
+        uint8_t dst = 0xff;
+        uint8_t ext_words = 0;
+
+        ptr = EMIT_LoadFromEffectiveAddress(ptr, size, &src, 0x18 | (opcode & 7), *m68k_ptr, &ext_words);
+        ptr = EMIT_LoadFromEffectiveAddress(ptr, size, &dst, 0x18 | ((opcode >> 9) & 7), *m68k_ptr, &ext_words);
+
+        switch (size)
+        {
+        case 4:
+            *ptr++ = rsbs_reg(src, src, dst, 0);
+            break;
+        case 2:
+            *ptr++ = lsl_immed(src, src, 16);
+            *ptr++ = rsbs_reg(src, src, dst, 16);
+            break;
+        case 1:
+            *ptr++ = lsl_immed(src, src, 24);
+            *ptr++ = rsbs_reg(src, src, dst, 24);
+            break;
+        }
+
+        RA_FreeARMRegister(&ptr, src);
+        RA_FreeARMRegister(&ptr, dst);
+
+        ptr = EMIT_AdvancePC(ptr, 2 * (ext_words + 1));
+        (*m68k_ptr) += ext_words;
+
+        uint8_t mask = M68K_GetSRMask(BE16((*m68k_ptr)[0]));
+        uint8_t update_mask = (SR_C | SR_V | SR_Z | SR_N) & ~mask;
+
+        if (update_mask)
+        {
+            *ptr++ = bic_immed(REG_SR, REG_SR, update_mask);
+            if (update_mask & SR_N)
+                *ptr++ = orr_cc_immed(ARM_CC_MI, REG_SR, REG_SR, SR_N);
+            if (update_mask & SR_Z)
+                *ptr++ = orr_cc_immed(ARM_CC_EQ, REG_SR, REG_SR, SR_Z);
+            if (update_mask & SR_V)
+                *ptr++ = orr_cc_immed(ARM_CC_VS, REG_SR, REG_SR, SR_V);
+            if (update_mask & SR_C) /* Note - after sub/rsb C flag on ARM is inverted! */
+                *ptr++ = orr_cc_immed(ARM_CC_CC, REG_SR, REG_SR, SR_C);
+        }
+    }
+    /* 1011xxxx11xxxxxx - CMPA */
+    else if ((opcode & 0xf0c0) == 0xb0c0)
+    {
+        uint8_t size = ((opcode >> 8) & 1) ? 4 : 2;
+        uint8_t src = 0xff;
+        uint8_t dst = RA_MapM68kRegister(&ptr, 8 + ((opcode >> 9) & 7));
+        uint8_t ext_words = 0;
+
+        ptr = EMIT_LoadFromEffectiveAddress(ptr, size, &src, opcode & 0x3f, *m68k_ptr, &ext_words);
+
+        switch (size)
+        {
+        case 4:
+            *ptr++ = rsbs_reg(src, src, dst, 0);
+            break;
+        case 2:
+            *ptr++ = lsl_immed(src, src, 16);
+            *ptr++ = rsbs_reg(src, src, dst, 16);
+            break;
+        case 1:
+            *ptr++ = lsl_immed(src, src, 24);
+            *ptr++ = rsbs_reg(src, src, dst, 24);
+            break;
+        }
+
+        RA_FreeARMRegister(&ptr, src);
+
+        ptr = EMIT_AdvancePC(ptr, 2 * (ext_words + 1));
+        (*m68k_ptr) += ext_words;
+
+        uint8_t mask = M68K_GetSRMask(BE16((*m68k_ptr)[0]));
+        uint8_t update_mask = (SR_C | SR_V | SR_Z | SR_N) & ~mask;
+
+        if (update_mask)
+        {
+            *ptr++ = bic_immed(REG_SR, REG_SR, update_mask);
+            if (update_mask & SR_N)
+                *ptr++ = orr_cc_immed(ARM_CC_MI, REG_SR, REG_SR, SR_N);
+            if (update_mask & SR_Z)
+                *ptr++ = orr_cc_immed(ARM_CC_EQ, REG_SR, REG_SR, SR_Z);
+            if (update_mask & SR_V)
+                *ptr++ = orr_cc_immed(ARM_CC_VS, REG_SR, REG_SR, SR_V);
+            if (update_mask & SR_C) /* Note - after sub/rsb C flag on ARM is inverted! */
+                *ptr++ = orr_cc_immed(ARM_CC_CC, REG_SR, REG_SR, SR_C);
+        }
+    }
+    /* 1011xxx0xxxxxxxx - CMP */
+    else if ((opcode & 0xf100) == 0xb000)
+    {
+        uint8_t size = 1 << ((opcode >> 6) & 3);
+        uint8_t src = 0xff;
+        uint8_t dst = RA_MapM68kRegister(&ptr, (opcode >> 9) & 7);
+        uint8_t ext_words = 0;
+
+        ptr = EMIT_LoadFromEffectiveAddress(ptr, size, &src, opcode & 0x3f, *m68k_ptr, &ext_words);
+
+        switch(size)
+        {
+            case 4:
+                *ptr++ = rsbs_reg(src, src, dst, 0);
+                break;
+            case 2:
+                *ptr++ = lsl_immed(src, src, 16);
+                *ptr++ = rsbs_reg(src, src, dst, 16);
+                break;
+            case 1:
+                *ptr++ = lsl_immed(src, src, 24);
+                *ptr++ = rsbs_reg(src, src, dst, 24);
+                break;
+        }
+
+        RA_FreeARMRegister(&ptr, src);
+
+        ptr = EMIT_AdvancePC(ptr, 2 * (ext_words + 1));
+        (*m68k_ptr) += ext_words;
+
+        uint8_t mask = M68K_GetSRMask(BE16((*m68k_ptr)[0]));
+        uint8_t update_mask = (SR_C | SR_V | SR_Z | SR_N) & ~mask;
+
+        if (update_mask)
+        {
+            *ptr++ = bic_immed(REG_SR, REG_SR, update_mask);
+            if (update_mask & SR_N)
+                *ptr++ = orr_cc_immed(ARM_CC_MI, REG_SR, REG_SR, SR_N);
+            if (update_mask & SR_Z)
+                *ptr++ = orr_cc_immed(ARM_CC_EQ, REG_SR, REG_SR, SR_Z);
+            if (update_mask & SR_V)
+                *ptr++ = orr_cc_immed(ARM_CC_VS, REG_SR, REG_SR, SR_V);
+            if (update_mask & SR_C) /* Note - after sub/rsb C flag on ARM is inverted! */
+                *ptr++ = orr_cc_immed(ARM_CC_CC, REG_SR, REG_SR, SR_C);
+        }
+    }
     /* 1011xxxxxxxxxxxx - EOR */
-    if ((opcode & 0xf000) == 0xb000)
+    else if ((opcode & 0xf000) == 0xb000)
     {
         uint8_t size = 1 << ((opcode >> 6) & 3);
         uint8_t direction = (opcode >> 8) & 1; // 0: Ea+Dn->Dn, 1: Ea+Dn->Ea
