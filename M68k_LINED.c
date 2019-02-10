c11d13bb876fab98076d4155097c5dd05a0e2dc0
Michal Schulz <michal.schulz@gmx.de>
2019-02-10 11:19:26 +0100

    ADD/SUB implemented

--- M68k_LINED.c
+++ M68k_LINED.c
@@ -135,6 +135,144 @@ uint32_t *EMIT_lineD(uint32_t *ptr, uint16_t **m68k_ptr)
                 *ptr++ = orr_cc_immed(ARM_CC_CS, REG_SR, REG_SR, SR_X | SR_C);
         }
     }
+    /* ADD */
+    else if ((opcode & 0xf000) == 0xd000)
+    {
+        uint8_t size = 1 << ((opcode >> 6) & 3);
+        uint8_t direction = (opcode >> 8) & 1; // 0: Ea+Dn->Dn, 1: Ea+Dn->Ea
+        uint8_t ext_words = 0;
+
+        if (direction == 0)
+        {
+            uint8_t dest = RA_MapM68kRegister(&ptr, (opcode >> 9) & 7);
+            uint8_t src = 0;
+
+            RA_SetDirtyM68kRegister(&ptr, (opcode >> 9) & 7);
+            ptr = EMIT_LoadFromEffectiveAddress(ptr, size, &src, opcode & 0x3f, *m68k_ptr, &ext_words);
+
+            switch (size)
+            {
+            case 4:
+                *ptr++ = adds_reg(dest, dest, src, 0);
+                break;
+            case 2:
+                *ptr++ = lsl_immed(src, src, 16);
+                *ptr++ = adds_reg(src, src, dest, 16);
+                *ptr++ = lsr_immed(src, src, 16);
+                *ptr++ = bfi(dest, src, 0, 16);
+                break;
+            case 1:
+                *ptr++ = lsl_immed(src, src, 24);
+                *ptr++ = adds_reg(src, src, dest, 24);
+                *ptr++ = lsr_immed(src, src, 24);
+                *ptr++ = bfi(dest, src, 0, 8);
+                break;
+            }
+
+            RA_FreeARMRegister(&ptr, src);
+        }
+        else
+        {
+            uint8_t dest = 0xff;
+            uint8_t src = RA_MapM68kRegister(&ptr, (opcode >> 9) & 7);
+            uint8_t tmp = RA_AllocARMRegister(&ptr);
+            uint8_t mode = (opcode & 0x0038) >> 3;
+
+            ptr = EMIT_LoadFromEffectiveAddress(ptr, 0, &dest, opcode & 0x3f, *m68k_ptr, &ext_words);
+
+            /* Fetch data into temporary register, perform add, store it back */
+            switch (size)
+            {
+            case 4:
+                if (mode == 4)
+                {
+                    *ptr++ = ldr_offset_preindex(dest, tmp, -4);
+                    RA_SetDirtyM68kRegister(&ptr, 8 + (opcode & 7));
+                }
+                else
+                    *ptr++ = ldr_offset(dest, tmp, 0);
+
+                /* Perform calcualtion */
+                *ptr++ = adds_reg(tmp, tmp, src, 0);
+
+                /* Store back */
+                if (mode == 3)
+                {
+                    *ptr++ = str_offset_postindex(dest, tmp, 4);
+                    RA_SetDirtyM68kRegister(&ptr, 8 + (opcode & 7));
+                }
+                else
+                    *ptr++ = str_offset(dest, tmp, 0);
+                break;
+            case 2:
+                if (mode == 4)
+                {
+                    *ptr++ = ldrh_offset_preindex(dest, tmp, -2);
+                    RA_SetDirtyM68kRegister(&ptr, 8 + (opcode & 7));
+                }
+                else
+                    *ptr++ = ldrh_offset(dest, tmp, 0);
+                /* Perform calcualtion */
+                *ptr++ = lsl_immed(tmp, tmp, 16);
+                *ptr++ = adds_reg(tmp, tmp, src, 16);
+                *ptr++ = lsr_immed(tmp, tmp, 16);
+                /* Store back */
+                if (mode == 3)
+                {
+                    *ptr++ = strh_offset_postindex(dest, tmp, 2);
+                    RA_SetDirtyM68kRegister(&ptr, 8 + (opcode & 7));
+                }
+                else
+                    *ptr++ = strh_offset(dest, tmp, 0);
+                break;
+            case 1:
+                if (mode == 4)
+                {
+                    *ptr++ = ldrb_offset_preindex(dest, tmp, (opcode & 7) == 7 ? -2 : -1);
+                    RA_SetDirtyM68kRegister(&ptr, 8 + (opcode & 7));
+                }
+                else
+                    *ptr++ = ldrb_offset(dest, tmp, 0);
+
+                /* Perform calcualtion */
+                *ptr++ = lsl_immed(tmp, tmp, 24);
+                *ptr++ = adds_reg(tmp, tmp, src, 24);
+                *ptr++ = lsr_immed(tmp, tmp, 24);
+
+                /* Store back */
+                if (mode == 3)
+                {
+                    *ptr++ = strb_offset_postindex(dest, tmp, (opcode & 7) == 7 ? 2 : 1);
+                    RA_SetDirtyM68kRegister(&ptr, 8 + (opcode & 7));
+                }
+                else
+                    *ptr++ = strb_offset(dest, tmp, 0);
+                break;
+            }
+
+            RA_FreeARMRegister(&ptr, dest);
+            RA_FreeARMRegister(&ptr, tmp);
+        }
+
+        ptr = EMIT_AdvancePC(ptr, 2 * (ext_words + 1));
+        (*m68k_ptr) += ext_words;
+
+        uint8_t mask = M68K_GetSRMask(BE16((*m68k_ptr)[0]));
+        uint8_t update_mask = (SR_X | SR_C | SR_V | SR_Z | SR_N) & ~mask;
+
+        if (update_mask)
+        {
+            *ptr++ = bic_immed(REG_SR, REG_SR, update_mask);
+            if (update_mask & SR_N)
+                *ptr++ = orr_cc_immed(ARM_CC_MI, REG_SR, REG_SR, SR_N);
+            if (update_mask & SR_Z)
+                *ptr++ = orr_cc_immed(ARM_CC_EQ, REG_SR, REG_SR, SR_Z);
+            if (update_mask & SR_V)
+                *ptr++ = orr_cc_immed(ARM_CC_VS, REG_SR, REG_SR, SR_V);
+            if (update_mask & (SR_X | SR_C))
+                *ptr++ = orr_cc_immed(ARM_CC_CS, REG_SR, REG_SR, SR_X | SR_C);
+        }
+    }
 
     return ptr;
 }
