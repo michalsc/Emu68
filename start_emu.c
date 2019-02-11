b42dfb0bced25a6ab459efa9cdd6852951455d09
Michal Schulz <michal.schulz@tu-clausthal.de>
2019-02-11 12:02:49 +0100

    updated copyright headers in relevant files

--- start_emu.c
+++ start_emu.c
@@ -1,3 +1,12 @@
+/*
+    Copyright © 2019 Michal Schulz <michal.schulz@gmx.de>
+    https://github.com/michalsc
+
+    This Source Code Form is subject to the terms of the
+    Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+    with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 #define _GNU_SOURCE 1
 
 #include <stdio.h>
