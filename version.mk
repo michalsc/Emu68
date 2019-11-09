628658dfcf42a28bfe1bf1552c3fe9a637cf9266
Michal Schulz <michal.schulz@gmx.de>
2019-11-09 17:37:12 +0100

    append most recent git tag to $VER string

--- version.mk
+++ version.mk
@@ -1,10 +1,11 @@
 __GIT_HEAD   := $(shell git rev-parse --short HEAD)
 __BUILD_DATE := $(shell date +"%d.%m.%Y")
+__GIT_TAG    := $(shell git describe --tags --abbrev=0 | cut -dv -f2)
 
 ifeq (,$(shell git status --porcelain))
-VERSION_STRING_DATE := "($(__BUILD_DATE)) git: $(__GIT_HEAD)"
+VERSION_STRING_DATE := "$(__GIT_TAG) ($(__BUILD_DATE)) git: $(__GIT_HEAD)"
 else
-VERSION_STRING_DATE := "($(__BUILD_DATE)) git: $(__GIT_HEAD),dirty"
+VERSION_STRING_DATE := "$(__GIT_TAG) ($(__BUILD_DATE)) git: $(__GIT_HEAD),dirty"
 endif
 
 export VERSION_STRING_DATE
