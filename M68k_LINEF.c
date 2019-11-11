cd730777eedd7a6a2acc893245bf3e4e80e498e9
Michal Schulz <michal.schulz@gmx.de>
2019-11-11 07:25:04 +0100

    Cosine implemented in slightly different way

--- M68k_LINEF.c
+++ M68k_LINEF.c
@@ -27,6 +27,8 @@ enum {
     C_2_SQRTPI,
     C_SQRT2,
     C_SQRT1_2,
+    C_0_5,
+    C_1_5,
     C_LOG10_2 = 0x0b,
     C_E,
     C_LOG2E,
@@ -62,6 +64,8 @@ long double constants[128] = {
     [C_2_SQRTPI] =  M_2_SQRTPI,
     [C_SQRT2] =     M_SQRT2,
     [C_SQRT1_2] =   M_SQRT1_2,
+    [C_0_5] =       0.5,
+    [C_1_5] =       1.5,
     [C_LOG10_2] =   0.301029995663981195214, /* Official - Log10(2) */
     [C_E] =         M_E,                /* Official */
     [C_LOG2E] =     M_LOG2E,            /* Official */
@@ -69,7 +73,28 @@ long double constants[128] = {
     [C_ZERO] =      0.0,                /* Official */
 
     /* Polynom coefficients for sin(x) */
-#if USE_POLY_21
+
+    [C_SIN_COEFF] = -2.11100178050346585936E-5,
+                    4.65963708473294521719E-4,
+                    -7.37035513524020578156E-3,
+                    8.21458769726032277098E-2,
+                    -5.99264528627362954518E-1,
+                    2.55016403985097679243,
+                    -5.16771278004952168888,
+                    3.14159265358979102647,
+
+    [C_COS_COEFF] = 4.15383875943350535407E-6,
+                    -1.04570624685965272291E-4,
+                    1.92955784205552168426E-3,
+                    -2.58068890507489103003E-2,
+                    2.35330630164104256943E-1,
+                    -1.33526276884550367708,
+                    4.05871212641655666324,
+                    -4.93480220054467742126,
+                    9.99999999999999997244E-1,
+
+#if 0
+#if USE_POLY_21_
     [C_SIN_COEFF] = 1.71343967861184034706E-20,
                     -8.15103676569049647059E-18,
                     2.81031414820239505995E-15,
@@ -107,7 +132,8 @@ long double constants[128] = {
                     -1.38627507062573673756E-03,
                     4.16610337354021107429E-02,
                     -4.99995582499065048420E-01,
-                    9.99999443739537210853E-01
+                    9.99999443739537210853E-01,
+#endif
 #endif
 
     /* Polynom coefficients for cos(x) */
@@ -131,8 +157,8 @@ long double constants[128] = {
 };
 
 /*
-    Returns reminder of double number divided by 1, i.e. for any number it calculates result
-    of number mod 1. Used by trigonometric functions
+    Returns reminder of absolute double number divided by 2, i.e. for any number it calculates result
+    of number mod 2. Used by trigonometric functions
 */
 double TrimDoubleRange(double a)
 {
@@ -142,18 +168,15 @@ double TrimDoubleRange(double a)
         double d;
     } n, out;
 
-    int sign = 0;
     n.d = a;
-
-    sign = n.i32[0] & 0x80000000;
 
     uint32_t exp = (n.i32[0] >> 20) & 0x7ff;
     uint64_t man = n.i & 0x000fffffffffffffULL;
 
-    if (man && exp > 0x3fe && exp < (0x3ff + 52))
+    if (man && exp > 0x3ff && exp < (0x3ff + 52))
     {
-        man = (man << (exp - 0x3fe)) & 0x001fffffffffffffULL;
-        exp = 0x3fe;
+        man = (man << (exp - 0x3ff)) & 0x001fffffffffffffULL;
+        exp = 0x3ff;
 
         if (man) {
             int d = __builtin_clzll(man) - 11;
@@ -168,15 +191,13 @@ double TrimDoubleRange(double a)
             exp=0;
         }
     }
-    else if (!man && exp > 0x3fe)
+    else if (!man && exp > 0x3ff)
     {
         exp = 0;
     }
 
     out.i = man & ~0x0010000000000000ULL;
     out.i32[0] |= exp << 20;
-    if (sign)
-        out.i32[0] |= 0x80000000;
 
     return out.d;
 }
@@ -233,6 +254,14 @@ uint32_t *EMIT_lineF(uint32_t *ptr, uint16_t **m68k_ptr)
 
         /* Alloc destination FP register for write */
         fp_dst = RA_MapFPURegisterForWrite(&ptr, fp_dst);
+
+        *ptr++ = ldr_offset(15, base_reg, 0);
+        *ptr++ = b_cc(ARM_CC_AL, 0);
+        *ptr++ = BE32((uint32_t)(&constants[0]));
+
+        /* Multiply argument with 1/Pi - full 2Pi range reduces to 2 */
+        *ptr++ = fldd(fp_tmp1, base_reg, C_1_PI * 2);
+        *ptr++ = fmuld(fp_tmp2, fp_tmp1, fp_src);
 
         /*
             Load pointer to constants into base register, then load the value from table into
@@ -294,8 +323,14 @@ uint32_t *EMIT_lineF(uint32_t *ptr, uint16_t **m68k_ptr)
         uint8_t fp_dst = RA_MapFPURegister(&ptr, (opcode2 >> 7) & 7);
         uint8_t fp_src = RA_MapFPURegister(&ptr, (opcode2 >> 10) & 7);
         uint8_t base_reg = RA_AllocARMRegister(&ptr);
+        uint8_t top_half = RA_AllocARMRegister(&ptr);
+        uint8_t cmp_num = RA_AllocARMRegister(&ptr);
         uint8_t fp_tmp1 = RA_AllocFPURegister(&ptr);
         uint8_t fp_tmp2 = RA_AllocFPURegister(&ptr);
+        uint32_t *tmp_ptr;
+        uint32_t *exit_1;
+        uint32_t *exit_2;
+        uint32_t *exit_3;
 
         /* Alloc destination FP register for write */
         fp_dst = RA_MapFPURegisterForWrite(&ptr, fp_dst);
@@ -306,51 +341,153 @@ uint32_t *EMIT_lineF(uint32_t *ptr, uint16_t **m68k_ptr)
         */
         *ptr++ = ldr_offset(15, base_reg, 0);
         *ptr++ = b_cc(ARM_CC_AL, 0);
-        *ptr++ = BE32((uint32_t)(&constants[C_COS_COEFF]));
+        *ptr++ = BE32((uint32_t)(&constants[0]));
 
-#if USE_POLY_21
-        *ptr++ = fldd(fp_dst, base_reg, 0);        /* c0 -> tmp2 */
-        *ptr++ = fmuld(fp_tmp1, fp_src, fp_src);    /* Get tmp1 = x^2 */
-        *ptr++ = fldd(fp_tmp2, base_reg, 2);         /* c1 -> dst */
-        *ptr++ = fmacd(fp_tmp2, fp_dst, fp_tmp1);   /* c0 * x^2 + c1 -> dst */
-        *ptr++ = fldd(fp_dst, base_reg, 4);        /* c2 -> tmp2 */
-        *ptr++ = fmacd(fp_dst, fp_tmp2, fp_tmp1);   /* dst * x^2 + c2-> tmp2 */
-        *ptr++ = fldd(fp_tmp2, base_reg, 6);         /* c3 -> dst */
-        *ptr++ = fmacd(fp_tmp2, fp_dst, fp_tmp1);   /* tmp2 * x^2 + c3 -> dst */
-        *ptr++ = fldd(fp_dst, base_reg, 8);        /* c4 -> tmp2 */
-        *ptr++ = fmacd(fp_dst, fp_tmp2, fp_tmp1);   /* dst * x^2 + c4 -> tmp2 */
-        *ptr++ = fldd(fp_tmp2, base_reg, 10);        /* c5 -> dst */
-        *ptr++ = fmacd(fp_tmp2, fp_dst, fp_tmp1);   /* tmp2 * x^2 + c5 -> dst */
-        *ptr++ = fldd(fp_dst, base_reg, 12);       /* c6 -> tmp2 */
-        *ptr++ = fmacd(fp_dst, fp_tmp2, fp_tmp1);   /* dst * x^2 + c6 -> tmp2 */
-        *ptr++ = fldd(fp_tmp2, base_reg, 14);        /* c7 -> dst */
-        *ptr++ = fmacd(fp_tmp2, fp_dst, fp_tmp1);   /* tmp2 * x^2 + c7 -> dst */
-        *ptr++ = fldd(fp_dst, base_reg, 16);       /* c8 -> tmp2 */
-        *ptr++ = fmacd(fp_dst, fp_tmp2, fp_tmp1);   /* dst * x^2 + c8 -> tmp2 */
-        *ptr++ = fldd(fp_tmp2, base_reg, 18);        /* c9 -> dst */
-        *ptr++ = fmacd(fp_tmp2, fp_dst, fp_tmp1);   /* tmp2 * x^2 + c9 -> dst */
-        *ptr++ = fldd(fp_dst, base_reg, 20);       /* c10 -> tmp2 */
-        *ptr++ = fmacd(fp_dst, fp_tmp2, fp_tmp1);    /* dst * x^2 + c10 -> dst */
-#else
-        *ptr++ = fldd(fp_tmp2, base_reg, 0);        /* c0 -> tmp2 */
-        *ptr++ = fmuld(fp_tmp1, fp_src, fp_src);    /* Get tmp1 = x^2 */
-        *ptr++ = fldd(fp_dst, base_reg, 2);         /* c1 -> dst */
-        *ptr++ = fmacd(fp_dst, fp_tmp2, fp_tmp1);   /* c0 * x^2 + c1 -> dst */
-        *ptr++ = fldd(fp_tmp2, base_reg, 4);        /* c2 -> tmp2 */
-        *ptr++ = fmacd(fp_tmp2, fp_dst, fp_tmp1);   /* dst * x^2 + c2-> tmp2 */
-        *ptr++ = fldd(fp_dst, base_reg, 6);         /* c3 -> dst */
-        *ptr++ = fmacd(fp_dst, fp_tmp2, fp_tmp1);   /* tmp2 * x^2 + c3 -> dst */
-        *ptr++ = fldd(fp_tmp2, base_reg, 8);        /* c4 -> tmp2 */
-        *ptr++ = fmacd(fp_tmp2, fp_dst, fp_tmp1);   /* dst * x^2 + c4 -> tmp2 */
-        *ptr++ = fldd(fp_dst, base_reg, 10);        /* c5 -> dst */
-        *ptr++ = fmacd(fp_dst, fp_tmp2, fp_tmp1);   /* tmp2 * x^2 + c5 -> dst */
-#endif
+        *ptr++ = movw_immed_u16(cmp_num, 0);
+
+        /* cos(x)=cos(-x) -> tmp1 = |x| */
+        *ptr++ = fabsd(fp_tmp1, fp_src);
+
+        /* Divide x by Pi -> result into d0 */
+        *ptr++ = fldd(fp_tmp2, base_reg, C_1_PI * 2);
+        *ptr++ = fmuld(0, fp_tmp1, fp_tmp2);
+
+        /* Trim range to 0..2 */
+        /* Keep r0-r3,lr and ip safe on the stack. Exclude reg_quot and reg_rem in case they were allocated in r0..r4 range */
+        *ptr++ = push((1 << base_reg) | 0x0f | (1 << 12));
+        *ptr++ = ldr_offset(15, 12, 4);
+        *ptr++ = blx_cc_reg(ARM_CC_AL, 12);
+        *ptr++ = b_cc(ARM_CC_AL, 0);
+        *ptr++ = BE32((uint32_t)&TrimDoubleRange);
+        *ptr++ = pop((1 << base_reg) | 0x0f | (1 << 12));
+
+        /*
+            At this point d0 contains range trimmed to 0..2 corresponding to 0..2Pi.
+            Load top half of the d0 register into ARM register
+        */
+        *ptr++ = INSN_TO_LE(0xee300b10 | (top_half << 12));
+
+        /* Range 1: 0..0.5Pi - cos(x) uses cos(x) table */
+        *ptr++ = movt_immed_u16(cmp_num, 0x3fe0);
+        *ptr++ = cmp_reg(top_half, cmp_num);
+        tmp_ptr = ptr;
+        *ptr++ = b_cc(ARM_CC_GT, 0);
+
+        *ptr++ = fldd(fp_tmp1, base_reg, 2*C_COS_COEFF + 0);        /* c0 -> tmp1 */
+        *ptr++ = fmuld(0, 0, 0);
+        *ptr++ = fldd(fp_tmp2, base_reg, 2*C_COS_COEFF + 2);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp2, fp_tmp1, 0);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp1, base_reg, 2*C_COS_COEFF + 4);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp1, fp_tmp2, 0);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp2, base_reg, 2*C_COS_COEFF + 6);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp2, fp_tmp1, 0);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp1, base_reg, 2*C_COS_COEFF + 8);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp1, fp_tmp2, 0);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp2, base_reg, 2*C_COS_COEFF + 10);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp2, fp_tmp1, 0);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp1, base_reg, 2*C_COS_COEFF + 12);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp1, fp_tmp2, 0);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp2, base_reg, 2*C_COS_COEFF + 14);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp2, fp_tmp1, 0);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_dst, base_reg, 2*C_COS_COEFF + 16);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_dst, fp_tmp2, 0);                        /* c0 * x^2 + c1 -> dst */
+        exit_1 = ptr;
+        *ptr++ = b_cc(ARM_CC_AL, 0);
+        *tmp_ptr |= INSN_TO_LE(ptr - tmp_ptr - 2);
+
+        /* Range 2: 0.5Pi..1, cos(x) = -sin(x-0.5Pi) */
+        *ptr++ = movt_immed_u16(cmp_num, 0x3ff0);
+        *ptr++ = cmp_reg(top_half, cmp_num);
+        tmp_ptr = ptr;
+        *ptr++ = b_cc(ARM_CC_GT, 0);
+        *ptr++ = fldd(fp_tmp1, base_reg, C_0_5 * 2);
+        *ptr++ = fsubd(0, 0, fp_tmp1);
+        *ptr++ = fldd(fp_tmp1, base_reg, 2*C_SIN_COEFF + 0);        /* c0 -> tmp1 */
+        *ptr++ = fmuld(fp_dst, 0, 0);
+        *ptr++ = fldd(fp_tmp2, base_reg, 2*C_SIN_COEFF + 2);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp2, fp_tmp1, fp_dst);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp1, base_reg, 2*C_SIN_COEFF + 4);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp1, fp_tmp2, fp_dst);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp2, base_reg, 2*C_SIN_COEFF + 6);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp2, fp_tmp1, fp_dst);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp1, base_reg, 2*C_SIN_COEFF + 8);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp1, fp_tmp2, fp_dst);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp2, base_reg, 2*C_SIN_COEFF + 10);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp2, fp_tmp1, fp_dst);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp1, base_reg, 2*C_SIN_COEFF + 12);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp1, fp_tmp2, 0);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp2, base_reg, 2*C_SIN_COEFF + 14);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp2, fp_tmp1, fp_dst);
+        *ptr++ = fmuld(fp_dst, fp_dst, fp_tmp2);
+        *ptr++ = fnegd(fp_dst, fp_dst);
+        exit_2 = ptr;
+        *ptr++ = b_cc(ARM_CC_AL, 0);
+        *tmp_ptr |= INSN_TO_LE(ptr - tmp_ptr - 2);
+
+        /* Range 3: 1..1.5Pi, cos(x) = -cos(x-Pi) */
+        *ptr++ = movt_immed_u16(cmp_num, 0x3ff8);
+        *ptr++ = cmp_reg(top_half, cmp_num);
+        tmp_ptr = ptr;
+        *ptr++ = b_cc(ARM_CC_GT, 0);
+        *ptr++ = fldd(fp_tmp1, base_reg, C_10P0 * 2);
+        *ptr++ = fsubd(0, 0, fp_tmp1);
+        *ptr++ = fldd(fp_tmp1, base_reg, 2*C_COS_COEFF + 0);        /* c0 -> tmp1 */
+        *ptr++ = fmuld(0, 0, 0);
+        *ptr++ = fldd(fp_tmp2, base_reg, 2*C_COS_COEFF + 2);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp2, fp_tmp1, 0);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp1, base_reg, 2*C_COS_COEFF + 4);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp1, fp_tmp2, 0);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp2, base_reg, 2*C_COS_COEFF + 6);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp2, fp_tmp1, 0);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp1, base_reg, 2*C_COS_COEFF + 8);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp1, fp_tmp2, 0);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp2, base_reg, 2*C_COS_COEFF + 10);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp2, fp_tmp1, 0);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp1, base_reg, 2*C_COS_COEFF + 12);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp1, fp_tmp2, 0);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp2, base_reg, 2*C_COS_COEFF + 14);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp2, fp_tmp1, 0);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_dst, base_reg, 2*C_COS_COEFF + 16);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_dst, fp_tmp2, 0);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fnegd(fp_dst, fp_dst);
+        exit_3 = ptr;
+        *ptr++ = b_cc(ARM_CC_AL, 0);
+        *tmp_ptr |= INSN_TO_LE(ptr - tmp_ptr - 2);
+
+        /* Range 4: 1.5Pi..2Pi, cos(x) = sin(x-1.5Pi) */
+        *ptr++ = fldd(fp_tmp1, base_reg, C_1_5 * 2);
+        *ptr++ = fsubd(0, 0, fp_tmp1);
+        *ptr++ = fldd(fp_tmp1, base_reg, 2*C_SIN_COEFF + 0);        /* c0 -> tmp1 */
+        *ptr++ = fmuld(fp_dst, 0, 0);
+        *ptr++ = fldd(fp_tmp2, base_reg, 2*C_SIN_COEFF + 2);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp2, fp_tmp1, fp_dst);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp1, base_reg, 2*C_SIN_COEFF + 4);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp1, fp_tmp2, fp_dst);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp2, base_reg, 2*C_SIN_COEFF + 6);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp2, fp_tmp1, fp_dst);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp1, base_reg, 2*C_SIN_COEFF + 8);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp1, fp_tmp2, fp_dst);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp2, base_reg, 2*C_SIN_COEFF + 10);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp2, fp_tmp1, fp_dst);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp1, base_reg, 2*C_SIN_COEFF + 12);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp1, fp_tmp2, 0);                        /* c0 * x^2 + c1 -> dst */
+        *ptr++ = fldd(fp_tmp2, base_reg, 2*C_SIN_COEFF + 14);        /* c1 -> tmp2 */
+        *ptr++ = fmacd(fp_tmp2, fp_tmp1, fp_dst);
+        *ptr++ = fmuld(fp_dst, fp_dst, fp_tmp2);
+
+        *exit_1 |= INSN_TO_LE(ptr - exit_1 - 2);
+        *exit_2 |= INSN_TO_LE(ptr - exit_2 - 2);
+        *exit_3 |= INSN_TO_LE(ptr - exit_3 - 2);
 
         RA_FreeFPURegister(&ptr, fp_tmp1);
         RA_FreeFPURegister(&ptr, fp_tmp2);
         RA_FreeARMRegister(&ptr, base_reg);
+        RA_FreeARMRegister(&ptr, top_half);
+        RA_FreeARMRegister(&ptr, cmp_num);
         ptr = EMIT_AdvancePC(ptr, 2 * (ext_count + 1));
         (*m68k_ptr) += ext_count;
+
+        *ptr++ = INSN_TO_LE(0xfffffff0);
     }
     /* FNOP */
     else if (opcode == 0xf280 && opcode2 == 0)
