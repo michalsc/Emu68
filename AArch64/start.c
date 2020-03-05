cd7a079f81c0ac986314fd811be3be67744e6d6d
Michal Schulz <michal.schulz@gmx.de>
2020-03-05 20:36:25 +0100

    corrected load/store of status register from/to m68k context

--- AArch64/start.c
+++ AArch64/start.c
@@ -437,8 +437,7 @@ void M68K_LoadContext(struct M68KState *ctx)
     asm volatile("ldr d%0, %1"::"i"(REG_FP6),"m"(ctx->FP[6]));
     asm volatile("ldr d%0, %1"::"i"(REG_FP7),"m"(ctx->FP[7]));
 
-    uint64_t tmp_reg;
-    asm volatile("ldr %0, %1; msr tpidr_EL0, %0":"=r"(tmp_reg):"m"(ctx->SR));
+    asm volatile("ldrh w0, %0; msr tpidr_EL0, x0"::"m"(ctx->SR):"x0");
 }
 
 void M68K_SaveContext(struct M68KState *ctx)
@@ -472,8 +471,7 @@ void M68K_SaveContext(struct M68KState *ctx)
     asm volatile("str d%0, %1"::"i"(REG_FP6),"m"(ctx->FP[6]));
     asm volatile("str d%0, %1"::"i"(REG_FP7),"m"(ctx->FP[7]));
 
-    uint64_t tmp_reg;
-    asm volatile("mrs %0, tpidr_EL0; str %0, %1":"=r"(tmp_reg):"m"(ctx->SR));
+    asm volatile("mrs x0, tpidr_EL0; strh w0, %0"::"m"(ctx->SR):"x0");
 }
 
 void M68K_PrintContext(struct M68KState *m68k)
