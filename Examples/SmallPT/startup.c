92bdfeeeb9d6c48541b54fd3f1f0c5bf23224149
Michal Schulz <michal.schulz@gmx.de>
2020-03-06 17:48:31 +0100

    compile examples with -O2 and -mregparm=3

--- Examples/SmallPT/startup.c
+++ Examples/SmallPT/startup.c
@@ -1,5 +1,5 @@
 #include <stdint.h>
-
+#if 0
 asm("   .text\n"
 "       .globl _start\n"
 "_start: move.l %d2,-(%a7)\n"
@@ -11,7 +11,7 @@ asm("   .text\n"
 "       lea 16(sp), sp\n"
 "       rts"
 );
-
+#endif
 extern void SmallPT();
 extern uint16_t *framebuffer;
 extern uint32_t pitch;
@@ -21,8 +21,8 @@ void init_screen(uint16_t *fb, uint32_t p, uint32_t w, uint32_t h);
 void put_char(char);
 void silence(int);
 
-//void _c_start(uint32_t p asm("d0"), uint16_t *fb asm("a0"), uint32_t w asm("d1"), uint32_t h asm("d2"))
-void c_start(uint32_t p, uint16_t *fb, uint32_t w, uint32_t h)
+void _c_start(uint32_t p asm("d0"), uint16_t *fb asm("a0"), uint32_t w asm("d1"), uint32_t h asm("d2"))
+//void c_start(uint32_t p, uint16_t *fb, uint32_t w, uint32_t h)
 {
     silence(0);
     init_screen(fb, p, w, h);
