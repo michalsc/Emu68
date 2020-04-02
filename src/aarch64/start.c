47efe1fe3debcc649ecdc34e532e71a414999957
Michal Schulz <michal.schulz@gmx.de>
2020-04-02 07:50:13 +0200

    start experimenting with cmake

--- src/aarch64/start.c
+++ src/aarch64/start.c
@@ -7,6 +7,13 @@
     with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
+asm("   .section .startup           \n"
+"       .globl _start               \n"
+"       .globl _boot                \n"
+"       .type _start,%function      \n" /* Our kernel image starts with a standard header */
+"_boot: b       _boot               \n" /* code0: branch to the start */
+);
+#if 0
 #include <stdarg.h>
 #include <stdint.h>
 #include "A64.h"
@@ -46,7 +53,7 @@ asm("   .section .startup           \n"
 
 ".byte 0                            \n"
 ".align 4                           \n"
-".string \"$VER: Emu68.img " VERSION_STRING_DATE "\"\n"
+".string \"$VER: Emu68.img " VERSION_STRING "\"\n"
 ".byte 0                            \n"
 ".align 5                           \n"
 
@@ -609,3 +616,4 @@ void M68K_StartEmu(void *addr)
 
     M68K_DumpStats();
 }
+#endif
