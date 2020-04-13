d05031c224e91253cb67c9a95ed565086a177e39
Michal Schulz <michal.schulz@gmx.de>
2020-04-14 00:10:12 +0200

    dirty status is shared across all copies of the register

--- include/emu68/RegisterAllocator.h
+++ include/emu68/RegisterAllocator.h
@@ -17,22 +17,16 @@
 namespace emu68 {
 
 class INT {};
-class FPU {};
-class SR {};
+class DOUBLE {};
+class SINGLE {};
 
-enum class Role : uint8_t { TempReg, M68KReg, M68KAlias, M68KSpecial, TempConstant };
+enum RegisterRole : uint8_t { Dn=0, An=8, FPn = 16, PC = 24, SR, FPCR, FPSR, CTX, TempReg, TempConstant };
 
 template< typename Arch, typename RegType > class Register;
 
 template< typename Arch, typename RegType = INT >
 class RegisterAllocator {
 public:
-    Register< Arch, RegType > get() {
-        uint8_t regnum = __alloc_reg();
-        Register< Arch, RegType > reg(regnum);
-        reg._role = Role::TempReg;
-        return std::move(reg);
-    }
     uint8_t allocate() { return __alloc_reg(); }
     void deallocate(uint8_t r) { __free_reg(r); }
 private:
@@ -63,29 +57,37 @@ uint32_t RegisterAllocator<arch, reg>::_register_pool;
 
 template< typename Arch, typename RegType = INT >
 class Register {
-public:
-    Register(bool alloc=false) : _role(Role::TempReg), _dirty(false) { if (alloc) { _regnum=RegisterAllocator< Arch, RegType >().allocate(); _refcount = allocator<uint32_t>().allocate(1); (*_refcount) = 1;} else {_regnum=0xff; _refcount=nullptr;}}
-    Register(uint8_t regnum) : _regnum(regnum), _role(Role::TempReg), _dirty(false) { _refcount = allocator<uint32_t>().allocate(1); (*_refcount) = 1; }
-    Register(uint8_t regnum, Role role) : _regnum(regnum), _role(_role), _dirty(false) { _refcount = allocator<uint32_t>().allocate(1); (*_refcount) = 1; }
-    Register(Register& other) : _regnum(other._regnum), _role(other._role), _dirty(other._dirty), _refcount(other._refcount) { (*_refcount)++; }
-    Register(Register&& other) : _regnum(other._regnum), _role(other._role), _dirty(other._dirty), _refcount(other._refcount) { other._refcount = nullptr; other._regnum = 0xff; }
-    ~Register() { _decrease_and_release(); }
-    Register& operator=(Register& other) { _decrease_and_release(); _regnum = other._regnum; _role = other._role; _dirty = other._dirty; _refcount = other._refcount; (*_refcount)++; return *this; }
-    Register& operator=(const Register& other) { _decrease_and_release(); _regnum = other._regnum; _role = other._role; _dirty = other._dirty; _refcount = other._refcount; (*_refcount)++; return *this; }
-    Register& operator=(Register&& other) { _decrease_and_release(); _regnum = other._regnum; _role = other._role; _dirty = other._dirty; _refcount = other._refcount; other._refcount = nullptr; other._regnum = 0xff; return *this; }
-    Register& operator=(const Register&& other) { _decrease_and_release(); _regnum = other._regnum; _role = other._role; _dirty = other._dirty; _refcount = other._refcount; other._refcount = nullptr; other._regnum = 0xff; return *this; }
-    void alloc() { _decrease_and_release(); _regnum = RegisterAllocator< Arch, RegType >().allocate(); _role = Role::TempReg; _dirty = false; _refcount = allocator<uint32_t>().allocate(1); (*_refcount) = 1; }
-    uint8_t value() { if (_regnum == 0xff) { kprintf("[CXX:Register] Using unitialized register!\n"); } return _regnum; }
-    void touch() { _dirty = true; }
-    bool dirty() { return _dirty; }
 private:
-    void _decrease_and_release() { if (_refcount && --(*_refcount) == 0) { RegisterAllocator< Arch, RegType >().deallocate(_regnum); allocator<uint32_t>().deallocate(_refcount, 1); } }
+    struct _RefCountAndDirty {
+        uint32_t _cnt;
+        bool _dirty;
+    };
+public:
+    Register(bool alloc=false) : _role(RegisterRole::TempReg) { if (alloc) { _regnum=RegisterAllocator< Arch, RegType >().allocate(); _refcount = allocator<_RefCountAndDirty>().allocate(1); _refcount->_cnt = 1; _refcount->_dirty = false; } else {_regnum=0xff; _refcount=nullptr;}}
+    Register(uint8_t regnum) : _regnum(regnum), _role(RegisterRole::TempReg) { _refcount = allocator<_RefCountAndDirty>().allocate(1); _refcount->_cnt = 1; _refcount->_dirty = false; }
+    Register(uint8_t regnum, RegisterRole role) : _regnum(regnum), _role(role) { _refcount = allocator<_RefCountAndDirty>().allocate(1); _refcount->_cnt = 1; _refcount->_dirty = false; }
+    Register(Register& other) : _regnum(other._regnum), _role(other._role), _refcount(other._refcount) { _refcount->_cnt++; }
+    Register(Register&& other) : _regnum(other._regnum), _role(other._role), _refcount(other._refcount) { other._refcount = nullptr; other._regnum = 0xff; }
+    ~Register() { _decrease_and_release(); }
+    Register& operator=(Register& other) { _decrease_and_release(); _regnum = other._regnum; _role = other._role; _refcount = other._refcount; _refcount->_cnt++; return *this; }
+    Register& operator=(const Register& other) { _decrease_and_release(); _regnum = other._regnum; _role = other._role; _refcount = other._refcount; _refcount->_cnt++; return *this; }
+    Register& operator=(Register&& other) { _decrease_and_release(); _regnum = other._regnum; _role = other._role; _refcount = other._refcount; other._refcount = nullptr; other._regnum = 0xff; return *this; }
+    Register& operator=(const Register&& other) { _decrease_and_release(); _regnum = other._regnum; _role = other._role; _refcount = other._refcount; other._refcount = nullptr; other._regnum = 0xff; return *this; }
+    void alloc() { _decrease_and_release(); _regnum = RegisterAllocator< Arch, RegType >().allocate(); _role = RegisterRole::TempReg; _refcount = allocator<_RefCountAndDirty>().allocate(1); _refcount->_cnt = 1; _refcount->_dirty = false; }
+    bool allocated() { const int max = std::is_same<RegType, INT>::value ? Arch().RegEnd : Arch().FPURegEnd; const int min = std::is_same<RegType, INT>::value ? Arch().RegStart : Arch().FPURegStart; if ((_regnum >= min) && (_regnum <= max)) {return true;} else { return false;} }
+    uint8_t value() { if (_regnum == 0xff) { kprintf("[CXX:Register] Using unitialized register!\n"); } return _regnum; }
+    bool valid() { return _regnum != 0xff; }
+    void touch() { if (_refcount) { _refcount->_dirty = true; } }
+    bool dirty() { if (_refcount) { return _refcount->_dirty; } else return false; }
+    RegisterRole role() { return _role; }
+private:
+    void _decrease_and_release() { if (_refcount && --(_refcount->_cnt) == 0) { RegisterAllocator< Arch, RegType >().deallocate(_regnum); allocator<_RefCountAndDirty>().deallocate(_refcount, 1); } }
     uint8_t _regnum;
-    Role _role;
-    bool _dirty;
-    uint32_t* _refcount;
+    RegisterRole _role;
+    _RefCountAndDirty *_refcount;
     friend class RegisterAllocator< Arch, RegType >;
 };
+
 
 }
 
