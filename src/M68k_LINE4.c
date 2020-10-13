6b3cefde9a89b38ce9912d19e4b5fa0f97682c81
Michal Schulz <michal@Michals-iMac-Pro.local>
2020-10-13 16:30:03 +0200

    MOVE USP implemented

--- src/M68k_LINE4.c
+++ src/M68k_LINE4.c
@@ -1214,9 +1214,75 @@ uint32_t *EMIT_line4(uint32_t *ptr, uint16_t **m68k_ptr)
     /* 010011100110xxxx - MOVE USP */
     else if ((opcode & 0xfff0) == 0x4e60)
     {
+#ifdef __aarch64__
+        uint32_t *tmp;
+        ptr = EMIT_FlushPC(ptr);
+        uint8_t ctx = RA_GetCTX(&ptr);
+        uint8_t cc = RA_ModifyCC(&ptr);
+        uint8_t an = RA_MapM68kRegister(&ptr, 8 + (opcode & 7));
+
+        /* Test if supervisor mode is active */
+        *ptr++ = ands_immed(31, cc, 1, 32 - SRB_S);
+    
+        /* Branch to exception if not in supervisor */
+        *ptr++ = b_cc(A64_CC_EQ, 4);
+        if (opcode & 8)
+        {
+            *ptr++ = ldr_offset(ctx, an, __builtin_offsetof(struct M68KState, USP));
+            RA_SetDirtyM68kRegister(&ptr, 8 + (opcode & 7));
+        }
+        else
+        {
+            *ptr++ = str_offset(ctx, an, __builtin_offsetof(struct M68KState, USP));
+        }
+        *ptr++ = add_immed(REG_PC, REG_PC, 2);
+        tmp = ptr;
+        *ptr++ = b_cc(A64_CC_AL, 10);
+
+        /* No supervisor. Update USP, generate exception */
+        {
+            uint8_t ctx = RA_GetCTX(&ptr);
+            uint8_t sp = RA_MapM68kRegister(&ptr, 15);
+            uint8_t vbr = RA_AllocARMRegister(&ptr);
+
+            RA_SetDirtyM68kRegister(&ptr, 15);
+
+            /* Store A7 as USP */
+            *ptr++ = str_offset(ctx, sp, __builtin_offsetof(struct M68KState, USP));
+            /* Load ISP to A7 */
+            *ptr++ = ldr_offset(ctx, sp, __builtin_offsetof(struct M68KState, ISP));
+
+            /* Store exception vector and type */
+            *ptr++ = mov_immed_u16(vbr, 32, 0);
+            *ptr++ = strh_offset_preindex(sp, vbr, -2);
+
+            /* Store program counter */
+            *ptr++ = str_offset_preindex(sp, REG_PC, -4);
+
+            /* Store SR */
+            *ptr++ = strh_offset_preindex(sp, cc, -2);
+
+            /* Clear trace flags, set supervisor */
+            *ptr++ = bic_immed(cc, cc, 2, 32 - SRB_T0);
+            *ptr++ = orr_immed(cc, cc, 1, 32 - SRB_S);
+
+            /* Load VBR */
+            *ptr++ = ldr_offset(ctx, vbr, __builtin_offsetof(struct M68KState, VBR));
+            *ptr++ = ldr_offset(vbr, REG_PC, 32);
+
+            RA_FreeARMRegister(&ptr, vbr);
+        }
+
+        *tmp = b_cc(A64_CC_AL, ptr - tmp);
+        *ptr++ = (uint32_t)(uintptr_t)tmp;
+        *ptr++ = 1;
+        *ptr++ = 0;
+        *ptr++ = INSN_TO_LE(0xfffffffe);
+#else
         ptr = EMIT_InjectDebugString(ptr, "[JIT] MOVE USP at %08x not implemented\n", *m68k_ptr - 1);
         ptr = EMIT_InjectPrintContext(ptr);
         *ptr++ = udf(opcode);
+#endif
     }
     /* 0100111001110000 - RESET */
     else if (opcode == 0x4e70)
