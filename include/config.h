7594fbfcd3e47313c0dea6d6704ae073bbb93765
Michal Schulz <michal@Michals-iMac-Pro.local>
2020-10-19 08:57:27 +0200

    increase JIT cache size

--- include/config.h
+++ include/config.h
@@ -35,7 +35,7 @@
 #endif
 
 #define KERNEL_SYS_PAGES        2
-#define KERNEL_JIT_PAGES        8
+#define KERNEL_JIT_PAGES        16
 #define KERNEL_RSRVD_PAGES      ((KERNEL_JIT_PAGES) + (KERNEL_SYS_PAGES))
 
 #endif /* _CONFIG_H */
