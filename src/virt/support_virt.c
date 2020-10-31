4c131b71d6245d535332333e7a6b1f068bc44fba
Michal Schulz <michal@Michals-iMac-Pro.local>
2020-10-31 14:19:40 +0100

    serial port for virt machine. Proper Initialization is missing

--- src/virt/support_virt.c
+++ src/virt/support_virt.c
@@ -14,32 +14,46 @@
 
 static int serial_up = 0;
 
+#define UARTDR      0x000
+#define UARTRSR     0x004
+#define UARTECR     0x004
+#define UARTFR      0x018
+#define UARTILPR    0x020
+#define UARTIBRD    0x024
+#define UARTFBRD    0x028
+
+#define UARTFR_CTS  0x001
+#define UARTFR_DSR  0x002
+#define UARTFR_DCD  0x004
+#define UARTFR_BUSY 0x008
+#define UARTFR_RXFE 0x010
+#define UARTFR_TXFF 0x020
+#define UARTFR_RXFF 0x040
+#define UARTFR_TXFE 0x080
+#define UARTFR_RI   0x100
+
 static inline void waitSerOUT(void *io_base)
 {
     (void)io_base;
     while(1)
     {
-//       if ((rd32le((intptr_t)io_base + UART_LSR) & (1 << 5)) != 0) break;
+       if ((rd32le((intptr_t)io_base + UARTFR) & UARTFR_TXFF) == 0) break;
     }
 }
 
 static inline void putByte(void *io_base, char chr)
 {
-    (void)io_base;
-    (void)chr;
     if (serial_up)
     {
-/*
         waitSerOUT(io_base);
 
         if (chr == '\n')
         {
-            wr32le((intptr_t)io_base + UART_THR, '\r');
+            wr32le((intptr_t)io_base + UARTDR, '\r');
             waitSerOUT(io_base);
         }
-        wr32le((intptr_t)io_base + UART_THR, (uint8_t)chr);
+        wr32le((intptr_t)io_base + UARTDR, (uint8_t)chr);
         waitSerOUT(io_base);
-*/
     }
 }
 
@@ -47,28 +61,38 @@ void kprintf(const char * restrict format, ...)
 {
     va_list v;
     va_start(v, format);
-    vkprintf_pc(putByte, (void*)0xff1a0000, format, v);
+    vkprintf_pc(putByte, (void*)0x09000000, format, v);
     va_end(v);
 }
 
 void vkprintf(const char * restrict format, va_list args)
 {
-    vkprintf_pc(putByte, (void*)0xff1a0000, format, args);
+    vkprintf_pc(putByte, (void*)0x09000000, format, args);
 }
 
 void setup_serial()
 {
-/*
-    uint32_t tmp;
-
-    tmp = rd32le(0xff1a0000 + UART_LCR);
-
-    while(rd32le(0xff1a0000 + UART_USR) & 1);
-    wr32le(0xff1a0000 + UART_LCR, tmp | 0x80);
-    wr32le(0xff1a0000 + UART_DLL, 13);
-    wr32le(0xff1a0000 + UART_DLH, 0);
-    wr32le(0xff1a0000 + UART_LCR, tmp);
-
     serial_up = 1;
+/*
+	of_node_t *aliases = dt_find_node("/aliases");
+    if (aliases)
+    {
+        of_property_t *seralias = dt_find_property(aliases, "serial1");
+        if (seralias)
+        {
+            of_node_t *serial = dt_find_node(seralias->op_value);
+
+            if (serial)
+            {
+                ULONG *reg = dt_find_property(serial, "reg")->op_value;
+                IPTR phys_base = reg[0];
+                ULONG *ranges = dt_find_property(serial->on_parent, "ranges")->op_value;
+                phys_base -= ranges[0];
+                phys_base += ranges[1];
+                
+                pl011_base = (void *)phys_base;
+            }
+        }
+    }
 */
 }
