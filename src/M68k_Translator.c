b53182c8fa7aa616ec463e1ff6c7340a7d075acf
Michal Schulz <michal.schulz@gmx.de>
2021-08-10 17:03:00 +0200

    Function signature for Line A fixed

--- src/M68k_Translator.c
+++ src/M68k_Translator.c
@@ -100,10 +100,12 @@ uint32_t *EMIT_ResetOffsetPC(uint32_t *ptr)
     return ptr;
 }
 
-uint32_t *EMIT_lineA(uint32_t *arm_ptr, uint16_t **m68k_ptr)
+uint32_t *EMIT_lineA(uint32_t *arm_ptr, uint16_t **m68k_ptr, uint16_t *insn_consumed)
 {
     uint16_t opcode = BE16((*m68k_ptr)[0]);
     (*m68k_ptr)++;
+
+    (*insn_consumed)++;
 
     arm_ptr = EMIT_FlushPC(arm_ptr);
     arm_ptr = EMIT_InjectDebugString(arm_ptr, "[JIT] LINE A exception (opcode %04x) at %08x not implemented\n", opcode, *m68k_ptr - 1);
