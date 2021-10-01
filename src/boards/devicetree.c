1866f4dbfc7a7df58adeac6a2ad4f54002d17272
Michal Schulz <michal.schulz@gmx.de>
2021-10-01 23:02:03 +0200

    board's rom file must use phys address for mapping. And also aligned to page size...

--- src/boards/devicetree.c
+++ src/boards/devicetree.c
@@ -8,8 +8,8 @@
 static void map(struct ExpansionBoard *board)
 {
     kprintf("[BOARD] Mapping ZIII devicetree board at address %08x\n", board->map_base);
-    mmu_map((uintptr_t)board->rom_file, board->map_base, board->rom_size, MMU_ACCESS | MMU_ISHARE | MMU_ALLOW_EL0 | MMU_READ_ONLY | MMU_ATTR(0), 0);
-    mmu_map(mmu_virt2phys((uintptr_t)dt_fdt_base()), board->map_base + board->rom_size, (dt_total_size() + 4095) & ~4096, MMU_ACCESS | MMU_ISHARE | MMU_ALLOW_EL0 | MMU_READ_ONLY | MMU_ATTR(0), 0);
+    mmu_map(mmu_virt2phys((uintptr_t)board->rom_file), board->map_base, board->rom_size, MMU_ACCESS | MMU_ISHARE | MMU_ALLOW_EL0 | MMU_READ_ONLY | MMU_ATTR(0), 0);
+    mmu_map(mmu_virt2phys((uintptr_t)dt_fdt_base()), board->map_base + board->rom_size, (dt_total_size() + 4095) & ~4095, MMU_ACCESS | MMU_ISHARE | MMU_ALLOW_EL0 | MMU_READ_ONLY | MMU_ATTR(0), 0);
 }
 
 static struct ExpansionBoard board = {
