8ce55ccca6c3f5e2e2a2d148e8253bf9c6615d12
Michal Schulz <michal.schulz@gmx.de>
2021-10-01 21:34:22 +0200

    header for autoconfig boards added

---
+++ include/boards.h
@@ -0,0 +1,14 @@
+#ifndef _BOARDS_H
+#define _BOARDS_H
+
+#include <stdint.h>
+
+struct ExpansionBoard {
+    const void *    rom_file;
+    uint32_t        rom_size;
+    uint32_t        map_base;
+    uint32_t        is_z3;
+    void            (*map)(struct ExpansionBoard *);
+};
+
+#endif /* _BOARDS_H */
