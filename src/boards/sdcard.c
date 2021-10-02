bb8bfd26a17602053cf2715a3d01b22da911d682
Michal Schulz <michal.schulz@gmx.de>
2021-10-02 14:57:04 +0200

    sdcard board added. Doesn't do much, yet

---
+++ src/boards/sdcard.c
@@ -0,0 +1,27 @@
+#include <boards.h>
+#include <mmu.h>
+#include <A64.h>
+#include <support.h>
+#include "./sdcard.h"
+
+/*
+    This is a Z3 ROM board with device tree resource. It provides userspace to read the keys and properties from
+    the tree in order to e.g. find the available peripherals. The board is provided with its own m68k ROM with the
+    resource inside. No ARM-side code is used in this board.
+*/
+
+static void map(struct ExpansionBoard *board)
+{
+    kprintf("[BOARD] Mapping ZIII sdcard board at address %08x\n", board->map_base);
+    mmu_map(mmu_virt2phys((uintptr_t)board->rom_file), board->map_base, board->rom_size, MMU_ACCESS | MMU_ISHARE | MMU_ALLOW_EL0 | MMU_READ_ONLY | MMU_ATTR(0), 0);
+}
+
+static struct ExpansionBoard board = {
+    sdcard_bin,
+    4096,
+    0,
+    1,
+    map
+};
+
+static void * __attribute__((used, section(".boards.z3"))) _board = &board;
