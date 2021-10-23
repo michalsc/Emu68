8c1733ca6e27a31f322fbb8f704be3ee9b459701
Michal Schulz <michal.schulz@gmx.de>
2021-10-23 15:42:46 +0200

    SBCD.B mem fixed

--- src/M68k_LINE8.c
+++ src/M68k_LINE8.c
@@ -434,12 +434,14 @@ uint32_t *EMIT_SBCD_reg(uint32_t *ptr, uint16_t opcode, uint16_t **m68k_ptr)
 uint32_t *EMIT_SBCD_mem(uint32_t *ptr, uint16_t opcode, uint16_t **m68k_ptr)
 {
     uint8_t update_mask = M68K_GetSRMask(*m68k_ptr - 1);
-    
     uint8_t tmp_a = RA_AllocARMRegister(&ptr);
     uint8_t tmp_b = RA_AllocARMRegister(&ptr);
-    uint8_t cc = RA_ModifyCC(&ptr);
+    uint8_t tmp_c = RA_AllocARMRegister(&ptr);
+    uint8_t tmp_d = RA_AllocARMRegister(&ptr);
+    uint8_t tmp_n = RA_AllocARMRegister(&ptr);
     uint8_t src = RA_AllocARMRegister(&ptr);
-    uint8_t dest = RA_AllocARMRegister(&ptr);
+    uint8_t cc = RA_ModifyCC(&ptr);
+
     uint8_t an_src = RA_MapM68kRegister(&ptr, 8 + (opcode & 7));
     uint8_t an_dest = RA_MapM68kRegister(&ptr, 8 + ((opcode >> 9) & 7));
 
@@ -449,10 +451,85 @@ uint32_t *EMIT_SBCD_mem(uint32_t *ptr, uint16_t opcode, uint16_t **m68k_ptr)
     else
         *ptr++ = ldrb_offset_preindex(an_src, src, -1);
     if (((opcode >> 9) & 7) == 7)
-        *ptr++ = ldrb_offset_preindex(an_dest, dest, -2);
+        *ptr++ = ldrb_offset_preindex(an_dest, tmp_n, -2);
     else
-        *ptr++ = ldrb_offset_preindex(an_dest, dest, -1);
+        *ptr++ = ldrb_offset_preindex(an_dest, tmp_n, -1);
 
+    *ptr++ = and_immed(tmp_a, src, 4, 0);   // Fetch low nibbles
+    *ptr++ = and_immed(tmp_b, tmp_n, 4, 0);
+
+    *ptr++ = and_immed(tmp_c, src, 4, 28);  // Fetch high nibbles
+    *ptr++ = and_immed(tmp_d, tmp_n, 4, 28);
+
+    // Test X flag
+    *ptr++ = tst_immed(cc, 1, 31 & (32 - SRB_X));   // Sub X
+
+    // Subtract nibbles
+    *ptr++ = sub_reg(tmp_a, tmp_b, tmp_a, LSL, 0);
+    *ptr++ = sub_reg(tmp_c, tmp_d, tmp_c, LSL, 0);
+    
+    // Perform subtraction on tmp_n test reg
+    *ptr++ = sub_reg(tmp_n, tmp_n, src, LSL, 0);
+
+    // if X was set (NE), decrease lower nibble result by one
+    *ptr++ = b_cc(A64_CC_EQ, 3);
+    *ptr++ = sub_immed(tmp_a, tmp_a, 1);
+    *ptr++ = sub_immed(tmp_n, tmp_n, 1);
+    if (update_mask & SR_XC)
+    {
+        *ptr++ = mov_reg(tmp_d, tmp_n);
+    }
+
+    // Join nibbles together in tmp_b register
+    *ptr++ = add_reg(tmp_b, tmp_a, tmp_c, LSL, 0);
+
+    // If lower libble overflowed, do radix correction
+    *ptr++ = ands_immed(31, tmp_a, 4, 28);
+    if (update_mask & SR_XC) {
+        *ptr++ = b_cc(A64_CC_EQ, 3);
+        *ptr++ = sub_immed(tmp_b, tmp_b, 6);
+        *ptr++ = sub_immed(tmp_d, tmp_d, 6);
+    }
+    else {
+        *ptr++ = b_cc(A64_CC_EQ, 2);
+        *ptr++ = sub_immed(tmp_b, tmp_b, 6);
+    }
+
+    // Check if result overflowed
+    *ptr++ = ands_immed(31, tmp_n, 1, 24);
+    *ptr++ = b_cc(A64_CC_EQ, 2);
+    *ptr++ = sub_immed(tmp_b, tmp_b, 0x60);
+
+    if (update_mask & SR_XC) {
+        *ptr++ = bic_immed(cc, cc, 1, 0);
+        *ptr++ = ands_immed(31, tmp_d, 2, 24);
+        *ptr++ = b_cc(A64_CC_EQ, 2);
+        *ptr++ = orr_immed(cc, cc, 1, 0);
+
+        if (update_mask & SR_X) {
+            *ptr++ = bfi(cc, cc, 4, 1);
+        }
+    }
+
+    // Insert result into target register
+    *ptr++ = strb_offset(an_dest, tmp_b, 0);
+
+    if (update_mask & SR_Z) {
+        *ptr++ = ands_immed(31, tmp_b, 8, 0);
+        *ptr++ = b_cc(A64_CC_EQ, 2);
+        *ptr++ = bic_immed(cc, cc, 1, 31 & (32 - SRB_Z));
+    }
+
+    RA_FreeARMRegister(&ptr, tmp_a);
+    RA_FreeARMRegister(&ptr, tmp_b);
+    RA_FreeARMRegister(&ptr, tmp_c);
+    RA_FreeARMRegister(&ptr, tmp_d);
+    RA_FreeARMRegister(&ptr, tmp_n);
+    RA_FreeARMRegister(&ptr, src);
+
+    ptr = EMIT_AdvancePC(ptr, 2);
+
+#if 0
     /* Operation */
     *ptr++ = ubfx(tmp_a, src, 0, 4);
     *ptr++ = ubfx(tmp_b, dest, 0, 4);
@@ -505,7 +582,7 @@ uint32_t *EMIT_SBCD_mem(uint32_t *ptr, uint16_t opcode, uint16_t **m68k_ptr)
     RA_FreeARMRegister(&ptr, tmp_b);
     RA_FreeARMRegister(&ptr, src);
     RA_FreeARMRegister(&ptr, dest);
-    ptr = EMIT_AdvancePC(ptr, 2);
+#endif
 
     return ptr;
 }
