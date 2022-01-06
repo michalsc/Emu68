6e35a006de7e0c4efa131f98f664b4417a13e6d7
Michal Schulz <michal.schulz@gmx.de>
2022-01-07 00:39:49 +0100

    exception generation slightly improved. Uses stacks stored in V31

--- src/M68k_Exception.c
+++ src/M68k_Exception.c
@@ -31,19 +31,37 @@ uint32_t *EMIT_Exception(uint32_t *ptr, uint16_t exception, uint8_t format, ...)
 
     RA_SetDirtyM68kRegister(&ptr, 15);
 
-    /* Check if we are changing stack due to user->supervisor transition */
-    *ptr++ = tbnz(cc, SRB_S, 6);
+    /* In case of privilege violation there is no need to check if we were in supervisor mode. We were not! */
+    if (exception == VECTOR_PRIVILEGE_VIOLATION) {
+        // Store USP
+        *ptr++ = mov_reg_to_simd(31, TS_S, 1, sp);
 
-    /* We were in user mode. Store A7 as USP */
-    *ptr++ = str_offset(ctx, sp, __builtin_offsetof(struct M68KState, USP));
+        /* Check if we need to load ISP or MSP */
+        *ptr++ = tbnz(cc, SRB_M, 3);
 
-    /* Check if we need to load ISP or MSP */
-    *ptr++ = tbnz(cc, SRB_M, 3);
+        /* Load ISP to A7 */
+        *ptr++ = mov_simd_to_reg(sp, 31, TS_S, 2);
+        *ptr++ = b(2);
+        /* Load MSP to A7 */
+        *ptr++ = mov_simd_to_reg(sp, 31, TS_S, 3);
+    }
+    else
+    {
+        /* Check if we are changing stack due to user->supervisor transition */
+        *ptr++ = tbnz(cc, SRB_S, 6);
 
-    /* Load ISP to A7 */
-    *ptr++ = ldr_offset(ctx, sp, __builtin_offsetof(struct M68KState, ISP));
-    *ptr++ = b(2);
-    *ptr++ = ldr_offset(ctx, sp, __builtin_offsetof(struct M68KState, MSP));
+        /* We were in user mode. Store A7 as USP */
+        *ptr++ = mov_reg_to_simd(31, TS_S, 1, sp);
+
+        /* Check if we need to load ISP or MSP */
+        *ptr++ = tbnz(cc, SRB_M, 3);
+
+        /* Load ISP to A7 */
+        *ptr++ = mov_simd_to_reg(sp, 31, TS_S, 2);
+        *ptr++ = b(2);
+        /* Load MSP to A7 */
+        *ptr++ = mov_simd_to_reg(sp, 31, TS_S, 3);
+    }
 
     if (format == 2 || format == 3)
     {
@@ -56,29 +74,30 @@ uint32_t *EMIT_Exception(uint32_t *ptr, uint16_t exception, uint8_t format, ...)
     }
     else if (format == 4)
     {
-        /* Format 2 and 3, store Effective address and address of faulting instruction */
+        /* Format 4, store Effective address and address of faulting instruction */
         uint32_t ea = va_arg(args, uint32_t);
-        *ptr++ = movw_immed_u16(vbr, ea & 0xffff);
-        if ((ea >> 16) != 0)
-            *ptr++ = movt_immed_u16(vbr, ea >> 16);
-        *ptr++ = str_offset_preindex(sp, vbr, -4);
+        uint32_t fault = va_arg(args, uint32_t);
 
-        ea = va_arg(args, uint32_t);
+        *ptr++ = movw_immed_u16(vbr, fault & 0xffff);
+        if ((fault >> 16) != 0)
+            *ptr++ = movt_immed_u16(vbr, fault >> 16);
+        *ptr++ = str_offset_preindex(sp, vbr, -8);
+
         *ptr++ = movw_immed_u16(vbr, ea & 0xffff);
         if ((ea >> 16) != 0)
             *ptr++ = movt_immed_u16(vbr, ea >> 16);
-        *ptr++ = str_offset_preindex(sp, vbr, -4);
+        *ptr++ = str_offset(sp, vbr, 4);
     }
+
+    /* Store SR */
+    *ptr++ = strh_offset_preindex(sp, cc, -8);
+
+    /* Store program counter */
+    *ptr++ = stur_offset(sp, REG_PC, 2);
 
     /* Store exception vector and type */
     *ptr++ = mov_immed_u16(vbr, (format << 12) | (exception & 0xfff), 0);
-    *ptr++ = strh_offset_preindex(sp, vbr, -2);
-
-    /* Store program counter */
-    *ptr++ = str_offset_preindex(sp, REG_PC, -4);
-
-    /* Store SR */
-    *ptr++ = strh_offset_preindex(sp, cc, -2);
+    *ptr++ = strh_offset(sp, vbr, 6);
 
     /* Clear trace flags, set supervisor */
     *ptr++ = bic_immed(cc, cc, 2, 32 - SRB_T0);
