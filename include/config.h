06c7d87f5c3c4f28842b30e742f4c8c5feac230b
Michal Schulz <michal.schulz@gmx.de>
2022-12-28 16:28:26 +0100

    bitbang values for ps32 updated

--- include/config.h
+++ include/config.h
@@ -44,11 +44,23 @@
 
 #ifdef PISTORM
 
+#ifdef PISTORM32
+
+#define PISTORM_BITBANG_DELAY       59
+#define PISTORM_CHIPSET_DELAY       12
+#define PISTORM_CIA_DELAY           0
+#define PISTORM_WRITE_BUFFER        0
+#define PISTORM_WRITE_BUFFER_SIZE   32  
+
+#else
+
 #define PISTORM_BITBANG_DELAY       21
 #define PISTORM_CHIPSET_DELAY       12
 #define PISTORM_CIA_DELAY           0
 #define PISTORM_WRITE_BUFFER        1
 #define PISTORM_WRITE_BUFFER_SIZE   32  
+
+#endif
 
 #endif
 
