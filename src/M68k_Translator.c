8b7452dc7baed1c34858ed04a8df7602355d38ec
Michal Schulz <michal.schulz@gmx.de>
2024-01-28 19:32:42 +0000

    don't use magic numbers (HASH parameters)

--- src/M68k_Translator.c
+++ src/M68k_Translator.c
@@ -43,7 +43,7 @@ static inline int globalDisasm() {
     return disasm;
 }
 
-struct List ICache[65536];
+struct List ICache[EMU68_HASHSIZE];
 struct List LRU;
 static uint32_t *temporary_arm_code;
 static struct M68KLocalState *local_state;
@@ -330,7 +330,7 @@ static inline uintptr_t M68K_Translate(uint16_t *m68kcodeptr)
     M68K_ResetReturnStack();
 
     if (debug) {
-        uint32_t hash_calc = (hash >> 5) & 0xffff;
+        uint32_t hash_calc = (hash >> EMU68_HASHSHIFT) & EMU68_HASHMASK;
         kprintf("[ICache] Creating new translation unit with hash %04x (m68k code @ %p)\n", hash_calc, (void*)m68kcodeptr);
         if (debug > 1)
             M68K_PrintContext(__m68k_state);
@@ -648,7 +648,7 @@ struct M68KTranslationUnit *M68K_GetTranslationUnit(uint16_t *m68kcodeptr)
     }
 
     /* Get 16-bit has from the pointer to m68k code */
-    hash = (hash >> 5) & 0xffff;
+    hash = (hash >> EMU68_HASHSHIFT) & EMU68_HASHMASK;
 
     if (debug > 2)
         kprintf("[ICache] GetTranslationUnit(%08x)\n[ICache] Hash: 0x%04x\n", (void*)m68kcodeptr, (int)hash);
