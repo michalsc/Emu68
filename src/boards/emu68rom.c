59f48ea77feb0a67d5c7e76db40b68da4b99c18c
Michal Schulz <michal.schulz@gmx.de>
2024-04-19 22:03:45 +0200

    Use one single support Z3 board with all drivers necessary for boot

--- src/boards/emu68rom.c
+++ src/boards/emu68rom.c
@@ -1,12 +1,33 @@
 #include <boards.h>
 #include <mmu.h>
 #include <A64.h>
-#include <devicetree.h>
 #include <support.h>
+#include <HunkLoader.h>
 #include <tlsf.h>
+#include <devicetree.h>
+#include <DuffCopy.h>
 
 __attribute__((aligned(4096)))
-#include "./devicetree.h"
+#include "./emu68rom.h"
+#include "./devicetree.resource.h"
+#include "./unicam.resource.h"
+#include "./brcm-sdhc.device.h"
+#include "./brcm-emmc.device.h"
+#include "./68040.library.h"
+
+static struct { 
+    void *rom_base;
+    uint32_t rom_size;
+    uint32_t load_size;
+} roms[] = {
+    { devicetree_resource, sizeof(devicetree_resource), 0},
+    { unicam_resource, sizeof(unicam_resource), 0 },
+    { brcm_sdhc_device, sizeof(brcm_sdhc_device), 0 },
+    { brcm_emmc_device, sizeof(brcm_emmc_device), 0 },
+    { __68040_library, sizeof(__68040_library), 0 },
+    
+    { NULL, 0, 0 }
+};
 
 /*
     This is a Z3 ROM board with device tree resource. It provides userspace to read the keys and properties from
@@ -14,14 +35,14 @@ __attribute__((aligned(4096)))
     resource inside. No ARM-side code is used in this board.
 */
 
-struct fdt_header *fdt_base;
+static struct fdt_header *fdt_base;
 static char *strings;
 static uint32_t strings_len;
 static uint32_t *data;
 static uint32_t data_len;
 static uint32_t allocated_len;
 
-void put_word(uint32_t word)
+static void put_word(uint32_t word)
 {
     while ((sizeof(uint32_t) * (data_len + 1)) > allocated_len)
     {
@@ -35,7 +56,7 @@ void put_word(uint32_t word)
     data[data_len++] = word;
 }
 
-void put_words(uint32_t *words, uint32_t count)
+static void put_words(uint32_t *words, uint32_t count)
 {
     while ((sizeof(uint32_t) * (data_len + count)) > allocated_len)
     {
@@ -52,7 +73,7 @@ void put_words(uint32_t *words, uint32_t count)
     }
 }
 
-int find_string(const char *str)
+static int find_string(const char *str)
 {
     uint32_t idx = 0;
 
@@ -73,7 +94,7 @@ int find_string(const char *str)
         return idx;
 }
 
-int add_string(const char *str)
+static int add_string(const char *str)
 {
     int idx = -1;
 
@@ -89,7 +110,7 @@ int add_string(const char *str)
     return idx;
 }
 
-void dump_node(of_node_t *node)
+static void dump_node(of_node_t *node)
 {
     of_property_t *prop;
     of_node_t *child;
@@ -122,7 +143,7 @@ void dump_node(of_node_t *node)
     put_word(FDT_END_NODE);
 }
 
-void build_fdt()
+static void build_fdt()
 {
     struct fdt_header *fdt_orig = dt_fdt_base();
     struct fdt_header fdt = *fdt_orig;
@@ -166,18 +187,68 @@ void build_fdt()
     tlsf_free(tlsf, strings);
 }
 
+/*
+    This is a Z3 ROM board with SDHC driver. More details can be found in the Emu68-tools repository.
+    The board is provided with its own m68k ROM with the driver inside. No ARM-side code is used in this board.
+*/
+
 static void map(struct ExpansionBoard *board)
 {
+    uint32_t total_length = 0;
+    void *rom_modules = 0;
+    void *virt_base = (void*)((uintptr_t)board->map_base + 0x1000);
+    
     build_fdt();
 
-    kprintf("[BOARD] Mapping ZIII devicetree board at address %08x\n", board->map_base);
+    kprintf("[BOARD] Mapping ZIII Emu68 ROM board at address %08x\n", board->map_base);
     mmu_map(mmu_virt2phys((uintptr_t)board->rom_file), board->map_base, board->rom_size, MMU_ACCESS | MMU_ISHARE | MMU_ALLOW_EL0 | MMU_READ_ONLY | MMU_ATTR_CACHED, 0);
-    mmu_map(mmu_virt2phys((uintptr_t)fdt_base), board->map_base + board->rom_size, (fdt_base->totalsize + 4095) & ~4095, MMU_ACCESS | MMU_ISHARE | MMU_ALLOW_EL0 | MMU_READ_ONLY | MMU_ATTR_CACHED, 0);
+
+    for (int i=0; roms[i].rom_base; i++)
+    {
+        roms[i].load_size = GetHunkFileSize(roms[i].rom_base);
+        //if (i == 0) {
+        //    roms[i].load_size = (roms[i].load_size + 4095) & ~4095;
+        //}
+
+        if (roms[i].load_size > 0) total_length += roms[i].load_size;
+    }
+
+    total_length += (fdt_base->totalsize + 255) & ~255;
+
+    total_length = (total_length + 4095) & ~4095;
+
+    rom_modules = tlsf_malloc_aligned(tlsf, total_length, 4096);
+
+    kprintf("[BOARD] Emu68 ROM needs %ld extra bytes, \n", total_length);
+    //kprintf("[BOARD]   buffer at %p\n", rom_modules);
+
+    mmu_map(mmu_virt2phys((uintptr_t)rom_modules), board->map_base + 0x1000, total_length, MMU_ACCESS | MMU_ISHARE | MMU_ALLOW_EL0 | MMU_READ_ONLY | MMU_ATTR_CACHED, 0);
+
+    for (int i=0; roms[i].rom_base; i++)
+    {
+        LoadHunkFile(roms[i].rom_base, rom_modules, virt_base);
+        kprintf("[BOARD] Loaded module to %08lx...%08lx\n", virt_base, virt_base + roms[i].load_size - 1);
+        
+        rom_modules = (void*)((uintptr_t)rom_modules + roms[i].load_size);
+        virt_base = (void*)((uintptr_t)virt_base + roms[i].load_size);
+
+        if (i == 0)
+        {
+            virt_base = (void*)((uintptr_t)virt_base - 0x18);
+            rom_modules = (void*)((uintptr_t)rom_modules - 0x18);
+            uint64_t size = (fdt_base->totalsize + 31) & ~31;
+            kprintf("[BOARD]   Copy FDT to %08lx...%08lx\n", virt_base, virt_base + size - 1);
+
+            DuffCopy(rom_modules, (const uint32_t *)fdt_base, size / 4);
+            rom_modules = (void*)((uintptr_t)rom_modules + size);
+            virt_base = (void*)((uintptr_t)virt_base + size);
+        }
+    }
 }
 
 static struct ExpansionBoard board = {
-    devicetree_bin,
-    4096,
+    emu68rom_bin,
+    sizeof(emu68rom_bin),
     0,
     1,
     1,
