d282e68b9425701898b8534657df3946a79b9436
Michal Schulz <michal.schulz@gmx.de>
2026-08-13 21:26:48 +0000

    lock more of critical registers

--- include/RegLock.h
+++ include/RegLock.h
@@ -6,5 +6,10 @@
 register uint64x2_t reserved_reg_q19 asm("q19");
 register uint64x2_t reserved_reg_q20 asm("q20");
 register uint64x2_t reserved_reg_q21 asm("q21");
+register uint64x2_t reserved_reg_q22 asm("q22");
+register uint64x2_t reserved_reg_q23 asm("q23");
+register uint64x2_t reserved_reg_q24 asm("q24");
+register uint64x2_t reserved_reg_q25 asm("q25");
+register uint64x2_t reserved_reg_q26 asm("q26");
 
 #endif /* _REGLOCK_H */
